ef370160c7209148e727ad5514b2337b15592869
Learner Getready <learnergit@hymyilee.fi>
2024-02-18 16:38:51 +0200

    11.9 with some cypress config rows silenced correctly in eslint

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('cypress') //eslint-disable-line no-undef
 
-module.exports = defineConfig({ //eslint-disable-next-line no-undef
+module.exports = defineConfig({ //eslint-disable-line no-undef
   e2e: {
-    setupNodeEvents(on, config) { //eslint-disable-next-line no-unused-vars
+    setupNodeEvents(on, config) { //eslint-disable-line no-unused-vars
       // implement node event listeners here
     },
   },
