270cf8437e37726f3a677c36d334bc0564dd4aa5
Learner Getready <learnergit@hymyilee.fi>
2024-02-23 20:20:30 +0200

    11.11 got green but now with new version number and fixed fly-actions version

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 const PORT = process.env.PORT || 5000 //eslint-disable-line no-undef
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('2') // change this string to ensure a new version deployed
 })
 
 app.use(express.static('dist'))
