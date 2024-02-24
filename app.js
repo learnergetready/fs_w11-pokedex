5e6b204dbdd9abc39dd29e132193357714f10600
Learner Getready <learnergit@hymyilee.fi>
2024-02-24 14:44:08 +0200

    11.12 version 5 with canary strategy and health endpoint operational and only one machine on app

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const PORT = process.env.PORT || 5000 //eslint-disable-line no-undef
 app.get('/version', (req, res) => {
   res.send('5') // change this string to ensure a new version deployed
 })
-/*
+
 app.get('/health', (req, res) => {
   res.send('ok')
 })
-*/
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
