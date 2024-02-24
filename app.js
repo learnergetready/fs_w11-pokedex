69db1791eaeba0750d6465f4663dbaf3076b72dd
Learner Getready <learnergit@hymyilee.fi>
2024-02-24 14:50:18 +0200

    11.12 version 6 with canary strategy and health endpoint throwing errors and only one machine on app

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const app = express()
 const PORT = process.env.PORT || 5000 //eslint-disable-line no-undef
 
 app.get('/version', (req, res) => {
-  res.send('5') // change this string to ensure a new version deployed
+  res.send('6') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
